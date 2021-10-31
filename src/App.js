5b3e408f628052aaf06c54d4b7b195ad5a59d974
muthu <mkandanmk7@gmail.com>
2021-10-31 12:51:18 +0530

    navbar initailized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TopNavbar from "./Topnavbar/TopNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Topnavbar/TopNavbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TopNavbar.css";
+
+function TopNavbar() {
+  return (
+    <>
+      <div className="topleft">left</div>
+      <div className="topcenter">center</div>
+      <div className="topright">right</div>
+    </>
+  );
+}
+
+export default TopNavbar;
