a38c0d3f6961217ab2fc2a57369f69be4450cfb4
muthu <mkandanmk7@gmail.com>
2021-11-04 20:04:09 +0530

    pozt

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -16,6 +16,18 @@ export default function Post() {
         </span>
         <span className="time">1 hour ago</span>
       </div>
+      <p className="post_desc">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure beatae ad
+        voluptatem repellendus vero, nobis consequuntur facilis nihil vitae
+        fugit minus nulla quis perferendis corporis? Provident est aliquid sint
+        nisi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure
+        beatae ad voluptatem repellendus vero, nobis consequuntur facilis nihil
+        vitae fugit minus nulla quis perferendis corporis? Provident est aliquid
+        sint nisi. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Iure beatae ad voluptatem repellendus vero, nobis consequuntur facilis
+        nihil vitae fugit minus nulla quis perferendis corporis? Provident est
+        aliquid sint nisi.
+      </p>
     </div>
   );
 }
