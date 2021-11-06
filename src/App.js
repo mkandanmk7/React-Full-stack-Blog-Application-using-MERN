6da6bdd3351fc620e092c84819159e1d4057a933
muthu <mkandanmk7@gmail.com>
2021-11-06 17:09:23 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Home from "./Pages/Home/Home";
 import TopNavbar from "./Components/Topnavbar/TopNavbar";
 import Single from "./Components/Single/Single";
@@ -6,18 +7,23 @@ import Write from "./Pages/Write/Write";
 import Settings from "./Pages/Settings/Settings";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <Router>
       <TopNavbar />
-      <Register />
-      {/* <Login /> */}
-      {/* <Settings /> */}
-      {/* <Write /> */}
-      {/* <Single /> */}
-      {/* <Home /> */}
-    </div>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
