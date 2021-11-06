320504dfff03594fc7894b21051b5790970c858a
muthu <mkandanmk7@gmail.com>
2021-11-06 13:16:25 +0530

    pages done

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Single from "./Components/Single/Single";
 import Write from "./Pages/Write/Write";
 import Settings from "./Pages/Settings/Settings";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <TopNavbar />
-      <Login />
+      <Register />
+      {/* <Login /> */}
       {/* <Settings /> */}
       {/* <Write /> */}
       {/* <Single /> */}
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import "./Register.css";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <span className="registerTitle">Register </span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter Your Username..."
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter Your email..."
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password..."
+        />
+        <button className="registerButton">register</button>
+      </form>
+      <button className="LoginButton">Login</button>
+    </div>
+  );
 }
 
 export default Register;
