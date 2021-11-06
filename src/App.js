3ad54e8cbef7e7921ca5f0a7514000e97fd5924a
muthu <mkandanmk7@gmail.com>
2021-11-06 10:56:25 +0530

    settings page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from "./Pages/Home/Home";
 import TopNavbar from "./Components/Topnavbar/TopNavbar";
 import Single from "./Components/Single/Single";
 import Write from "./Pages/Write/Write";
+import Settings from "./Pages/Settings/Settings";
 
 function App() {
   return (
     <div className="App">
       <TopNavbar />
-      <Write />
+      <Settings />
+      {/* <Write /> */}
       {/* <Single /> */}
       {/* <Home /> */}
     </div>
