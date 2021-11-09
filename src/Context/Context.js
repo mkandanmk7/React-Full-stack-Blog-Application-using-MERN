91bd7e662fa0d44467d85786fae3f2919526dbfd
muthu <mkandanmk7@gmail.com>
2021-11-09 21:42:17 +0530

    context api done

---
+++ src/Context/Context.js
@@ -0,0 +1,14 @@
+import { createContext, useReducer } from "react";
+import Reducer from "./Reducer";
+
+const INITIAL_STATE = {
+  user: null,
+  isFetching: false,
+  error: false,
+};
+
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE); // which state the reducer wil use here
+};
---
+++ src/Context/Reducer.js
@@ -0,0 +1,27 @@
+//compare with previous state , action here
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: false,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: false,
+        isFetching: false,
+        error: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
