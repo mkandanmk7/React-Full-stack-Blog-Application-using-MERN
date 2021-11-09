bcf6bb4966d8449fcade5d5241e250b3d0310410
muthu <mkandanmk7@gmail.com>
2021-11-09 15:56:03 +0530

    single post

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,19 +1,27 @@
 import react from "react";
 import "./Post.css";
 import cat from "../../image/cat.jpg";
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
   console.log(post);
   return (
-    <div className="post">
-      <img src={cat} alt="cat" />
+    <div className="post" key={post._id}>
+      {post.photo ? (
+        <img src={post.photo} alt="cat" />
+      ) : (
+        <img src={cat} alt="cat" />
+      )}
+
       <div className="post_info">
         <div className="post_categories">
           {post.categories.map((cate) => {
             return <span className="post_cate">{cate}</span>;
           })}
         </div>
-        <span className="post_title">{post.title}</span>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="post_title">{post.title}</span>
+        </Link>
         <span className="time">{new Date(post.createdAt).toDateString()}</span>
       </div>
       <p className="post_desc">{post.desc}</p>
--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SinglePost.css";
 import PostImg from "../../image/cat.jpg";
-import { Delete, Edit, Star } from "@material-ui/icons";
+import { Delete, Edit } from "@material-ui/icons";
+import { useLocation } from "react-router";
+import axios from "axios";
 
 function SinglePost() {
+  const [post, setPost] = useState({});
+  const location = useLocation();
+
+  // console.log(location.pathname.split("/")[2]);
+  const postId = location.pathname.split("/")[2];
+
+  const getSinglePost = async () => {
+    const res = await axios.get(
+      `https://muthu-blog-server-api.herokuapp.com/api/posts/${postId}`
+    );
+    // console.log(res.data.details);
+    setPost(res.data.details);
+  };
+  //postId useEffect
+  useEffect(() => {
+    getSinglePost();
+  }, [postId]);
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img src={PostImg} alt="" className="singlePostImg" />
-        <h1 className="singlePostTitle">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        </h1>
+        {post.photo ? (
+          <img src={post.photo} alt="" className="singlePostImg" />
+        ) : (
+          <img src={PostImg} alt="" className="singlePostImg" />
+        )}
+
+        <h1 className="singlePostTitle">{post.title}</h1>
         <div className="singlePostEdit">
           <Edit color="primary" />
           <Delete style={{ color: "tomato" }} />
         </div>
         <div className="singlePostInfo">
           <span className="author">
-            Author: <b>Muthu</b>{" "}
+            Author: <b>{post.username}</b>{" "}
           </span>
-          <span className="date">1 hour ago</span>
+          <span className="date">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="singlePostDesc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odit
-          fuga laborum illum. Possimus sapiente quaerat quis ea quas similique
-          natus, doloribus officia consectetur voluptas itaque doloremque!
-          Eaque, repudiandae molestiae!Lorem Lorem, ipsum dolor sit amet
-          consectetur adipisicing elit. Asperiores veritatis tempora iste
-          reiciendis voluptas eum, itaque, esse dolorem accusamus blanditiis
-          cumque cum aperiam corrupti pariatur perspiciatis ipsam officia fuga
-          in!Lorem
-        </p>
+        <p className="singlePostDesc">{post.desc}</p>
       </div>
     </div>
   );
