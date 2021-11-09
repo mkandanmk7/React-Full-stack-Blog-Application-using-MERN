3e99fdb6f08652f614424adcd2618d6bcce3fe30
muthu <mkandanmk7@gmail.com>
2021-11-09 21:47:32 +0530

    context provider done

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -11,4 +11,17 @@ export const Context = createContext(INITIAL_STATE);
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, INITIAL_STATE); // which state the reducer wil use here
+
+  return (
+    <Context.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 };
